088760598acf40d3a34d2bea9d43723e4a2cb864
chenzhengwei <chenzhengwei@inspur.com>
2019-07-24 16:31:38 +0800

    优化退出

--- demo/demo-provider/src/main/java/com/alibaba/dubbo/demo/provider/Provider.java
+++ demo/demo-provider/src/main/java/com/alibaba/dubbo/demo/provider/Provider.java
@@ -2,8 +2,6 @@ package com.alibaba.dubbo.demo.provider;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.util.Scanner;
-
 /**
  * Created by ken.lj on 2017/7/31.
  */
@@ -12,11 +10,8 @@ public class Provider {
     public static void main(String[] args) throws Exception{
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/dubbo-demo-provider.xml"});
         context.start();
-
-        Scanner scan = new Scanner(System.in);
-        String accept = scan.nextLine();
-        while (!accept.equals("exit")) {
-            accept = scan.nextLine();
+        while (true) {
+            Thread.sleep(10000);
         }
     }
 
