9c4bcc36075f28c0f9591defad1c2db8876b26d0
wys <sdu_wys@163.com>
2024-03-20 19:19:20 +0800

    fix: fix lint error

--- ui-vue3/src/api/mock/mockRoutingRule.ts
+++ ui-vue3/src/api/mock/mockRoutingRule.ts
@@ -18,25 +18,25 @@
 import Mock from 'mockjs'
 
 Mock.mock('/mock/routingRule/search', 'get', () => {
-    const total = Mock.mock('@integer(8, 1000)');
-    const list = [];
-    for (let i = 0; i < total; i++) {
-      list.push({
-        ruleName: 'app_' + Mock.mock('@string(2,10)'),
-        ruleGranularity: Mock.mock('@integer(80, 200)'),
-        enable: Mock.mock('@boolean'),
-        effectiveTime: Mock.mock('@datetime'),
-        protection: Mock.mock('@boolean'),
-      });
-    }
-    return {
-      code: 200,
-      message: 'success',
-      data: Mock.mock({
-        total: total,
-        curPage: 1,
-        pageSize: 10,
-        data: list
-      })
-    };
-  });
+  const total = Mock.mock('@integer(8, 1000)')
+  const list = []
+  for (let i = 0; i < total; i++) {
+    list.push({
+      ruleName: 'app_' + Mock.mock('@string(2,10)'),
+      ruleGranularity: Mock.mock('@integer(80, 200)'),
+      enable: Mock.mock('@boolean'),
+      effectiveTime: Mock.mock('@datetime'),
+      protection: Mock.mock('@boolean')
+    })
+  }
+  return {
+    code: 200,
+    message: 'success',
+    data: Mock.mock({
+      total: total,
+      curPage: 1,
+      pageSize: 10,
+      data: list
+    })
+  }
+})
