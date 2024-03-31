8e28ef6f521650a294acf278cbc982c1e1e1281e
helltab <helltab@163.com>
2024-03-31 22:09:39 +0800

    Merge branch 'master' of https://github.com/Helltab/dubbo-kubernetes into feature/ui/framework/vue3
    * 'master' of https://github.com/Helltab/dubbo-kubernetes:
      feat: add traffic page
      feat: add traffic page
      feat: add internationalization
      feat: add mock data
    
    # Conflicts:
    #	ui-vue3/src/components/SearchTable.vue

--- ui-vue3/src/api/service/metricInfo.ts
+++ ui-vue3/src/api/service/metricInfo.ts
@@ -15,13 +15,12 @@
  * limitations under the License.
  */
 
-
-import request from "@/base/http/request";
+import request from '@/base/http/request'
 
 export const queryPromSql = (params: any): Promise<any> => {
-    return request({
-        url: 'http://123.56.255.156:9090/api/v1/query',
-        method: 'get',
-        params
-    })
+  return request({
+    url: 'http://123.56.255.156:9090/api/v1/query',
+    method: 'get',
+    params
+  })
 }
