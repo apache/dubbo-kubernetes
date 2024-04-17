7ec9b403a1592fb52ff89ffde9dc691559d4e41b
jianyi-gronk <jianyi_gronk@163.com>
2024-04-18 02:08:48 +0800

    feat: complete form view page

--- ui-vue3/src/views/traffic/dynamicConfig/tabs/formView.vue
+++ ui-vue3/src/views/traffic/dynamicConfig/tabs/formView.vue
@@ -71,8 +71,8 @@
             :label="$t('flowControlDomain.ruleGranularity')"
             :labelStyle="{ fontWeight: 'bold' }"
           >
-            <a-select v-model:value="formViewData.basicInfo.ruleGranularity" style="min-width: 120px" disabled>
-              <a-select-option :value="formViewData.basicInfo.ruleGranularity">{{ formViewData.basicInfo.ruleGranularity }}</a-select-option>
+            <a-select v-model:value="formViewEdit.basicInfo.ruleGranularity" style="min-width: 120px" disabled>
+              <a-select-option :value="formViewEdit.basicInfo.ruleGranularity">{{ formViewData.basicInfo.ruleGranularity }}</a-select-option>
             </a-select>
           </a-descriptions-item>
 
@@ -80,67 +80,138 @@
             :label="$t('flowControlDomain.actionObject')"
             :labelStyle="{ fontWeight: 'bold' }"
           >
-            <a-input v-model:value="formViewData.basicInfo.actionObject" style="min-width: 300px" disabled />
+            <a-input v-model:value="formViewEdit.basicInfo.actionObject" style="min-width: 300px" disabled />
           </a-descriptions-item>
 
           <a-descriptions-item
             :label="$t('flowControlDomain.enabledState')"
             :labelStyle="{ fontWeight: 'bold' }"
           >
-            <a-switch v-model:checked="enabledState" checked-children="是" un-checked-children="否" />
+            <a-switch v-model:checked="formViewEdit.basicInfo.enabledState" checked-children="是" un-checked-children="否" />
           </a-descriptions-item>
         </a-descriptions>
       </div>
     </a-card>
 
-    <a-card
-      :title="'配置【' + (index + 1) + '】'"
-      class="dynamic-config-card"
-      v-for="(config, index) in formViewData.config"
-    >
-      <a-descriptions :column="2">
-        <a-descriptions-item
-          :label="$t('flowControlDomain.enabledState')"
-          :labelStyle="{ fontWeight: 'bold' }"
-        >
-          {{ config.enabledState ? '启用' : '不启用' }}
-        </a-descriptions-item>
-        <a-descriptions-item
-          :label="$t('flowControlDomain.endOfAction')"
-          :labelStyle="{ fontWeight: 'bold' }"
-        >
-          {{ config.endOfAction }}
-        </a-descriptions-item>
-        <a-descriptions-item
-          :label="$t('flowControlDomain.actuatingRange')"
-          :labelStyle="{ fontWeight: 'bold' }"
-          :span="2"
-        >
-          <a-input-group compact v-for="item in config.actuatingRange">
-            <a-input disabled :value="item.key" style="width: 200px" />
-            <a-input disabled :value="item.relation" style="width: 50px" />
-            <a-input disabled :value="item.value" style="width: 200px" />
-          </a-input-group>
-        </a-descriptions-item>
-        <a-descriptions-item
-          :label="$t('flowControlDomain.configurationItem')"
-          :labelStyle="{ fontWeight: 'bold' }"
-          :span="2"
-        >
-          <a-input-group compact v-for="item in config.configItem">
-            <a-input disabled :value="item.key" style="width: 200px" />
-            <a-input disabled :value="item.relation" style="width: 50px" />
-            <a-input disabled :value="item.value" style="width: 200px" />
-          </a-input-group>
-        </a-descriptions-item>
-      </a-descriptions>
-    </a-card>
+    <template v-if="!isEdit">
+      <a-card
+        v-for="config, index in formViewData.config"
+        :title="'配置【' + (index + 1) + '】'"
+        class="dynamic-config-card"
+      >
+        <a-descriptions :column="2">
+          <a-descriptions-item
+            :label="$t('flowControlDomain.enabledState')"
+            :labelStyle="{ fontWeight: 'bold' }"
+          >
+            {{ config.enabledState ? '启用' : '不启用' }}
+          </a-descriptions-item>
+          <a-descriptions-item
+            :label="$t('flowControlDomain.endOfAction')"
+            :labelStyle="{ fontWeight: 'bold' }"
+          >
+            {{ config.endOfAction }}
+          </a-descriptions-item>
+          <a-descriptions-item
+            :label="$t('flowControlDomain.actuatingRange')"
+            :labelStyle="{ fontWeight: 'bold' }"
+            :span="2"
+          >
+            <a-input-group compact v-for="item in config.actuatingRange" :key="item.key">
+              <a-input disabled :value="item.key" style="width: 200px" />
+              <a-input disabled :value="item.relation" style="width: 50px" />
+              <a-input disabled :value="item.value" style="width: 200px" />
+            </a-input-group>
+          </a-descriptions-item>
+          <a-descriptions-item
+            :label="$t('flowControlDomain.configurationItem')"
+            :labelStyle="{ fontWeight: 'bold' }"
+            :span="2"
+          >
+            <a-input-group compact v-for="item in config.configItem" :key="item.key">
+              <a-input disabled :value="item.key" style="width: 200px" />
+              <a-input disabled :value="item.relation" style="width: 50px" />
+              <a-input disabled :value="item.value" style="width: 200px" />
+            </a-input-group>
+          </a-descriptions-item>
+        </a-descriptions>
+      </a-card>
+    </template>
+    
+    <template v-else>
+      <a-card
+        v-for="config, index in formViewEdit.config"
+        :title="'配置【' + (index + 1) + '】'"
+        class="dynamic-config-card"
+      >
+        <a-descriptions :column="2">
+          <a-descriptions-item
+            :label="$t('flowControlDomain.enabledState')"
+            :labelStyle="{ fontWeight: 'bold' }"
+          >
+            <a-switch v-model:checked="config.enabledState" checked-children="是" un-checked-children="否" />
+          </a-descriptions-item>
+          <a-descriptions-item
+            :label="$t('flowControlDomain.endOfAction')"
+            :labelStyle="{ fontWeight: 'bold' }"
+          >
+            <a-radio-group v-model:value="config.endOfAction" :options="endOfActionOptions" />
+          </a-descriptions-item>
+          <a-descriptions-item
+            :label="$t('flowControlDomain.actuatingRange')"
+            :labelStyle="{ fontWeight: 'bold' }"
+            :span="2"
+          >
+            <div>
+              <a-select
+                ref="select"
+                v-model:value="config.actuatingRangeKeys"
+                style="width: 500px"
+                @change="handleChange(index, 'actuatingRange')"
+                mode="multiple"
+                :options="actuatingRangeArr.map((item) => ({ value: item }))"
+              />
+              <div v-for="item in config.actuatingRange" :key="item.key" style="margin-top: 20px;">
+                <a-input-group compact>
+                  <a-input disabled :value="item.key" style="width: 200px" />
+                  <a-input disabled :value="item.relation" style="width: 50px" />
+                  <a-input :value="item.value" style="width: 200px" />
+                </a-input-group>
+              </div>
+            </div>
+          </a-descriptions-item>
+          <a-descriptions-item
+            :label="$t('flowControlDomain.configurationItem')"
+            :labelStyle="{ fontWeight: 'bold' }"
+            :span="2"
+          >
+            <div>
+              <a-select
+                ref="select"
+                v-model:value="config.configItemKeys"
+                style="width: 500px"
+                @change="handleChange(index, 'configItem')"
+                mode="multiple"
+                :options="configItemArr.map((item) => ({ value: item }))"
+              />
+              <div v-for="item in config.configItem" :key="item.key" style="margin-top: 20px;">
+                <a-input-group compact>
+                  <a-input disabled :value="item.key" style="width: 200px" />
+                  <a-input disabled :value="item.relation" style="width: 50px" />
+                  <a-input :value="item.value" style="width: 200px" />
+                </a-input-group>
+              </div>
+            </div>
+          </a-descriptions-item>
+        </a-descriptions>
+      </a-card>
+    </template>
 
-    <a-button v-if="isEdit">增加配置</a-button>
+    <a-button v-if="isEdit" @click="addConfig">增加配置</a-button>
 
     <a-flex v-if="isEdit" style="margin-top: 30px">
       <a-button type="primary">确认</a-button>
-      <a-button style="margin-left: 30px">取消</a-button>
+      <a-button style="margin-left: 30px" @click="router.push('../../')">取消</a-button>
     </a-flex>
   </div>
 </template>
@@ -152,7 +223,7 @@ import { CopyOutlined } from '@ant-design/icons-vue'
 import { PRIMARY_COLOR } from '@/base/constants'
 import useClipboard from 'vue-clipboard3'
 import { message } from 'ant-design-vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 
 let __ = PRIMARY_COLOR
 const {
@@ -162,6 +233,7 @@ const {
 } = <ComponentInternalInstance>getCurrentInstance()
 
 const route = useRoute()
+const router = useRouter()
 const isEdit = ref(route.params.isEdit === '1')
 
 const toClipboard = useClipboard().toClipboard
@@ -183,6 +255,7 @@ const formViewData = ref({
     {
       enabledState: true,
       endOfAction: 'provider',
+      actuatingRangeKeys: ['address'],
       actuatingRange: [
         {
           key: 'address',
@@ -190,6 +263,7 @@ const formViewData = ref({
           value: '10.255.10.11'
         }
       ],
+      configItemKeys: ['retries'],
       configItem: [
         {
           key: 'retries',
@@ -201,7 +275,51 @@ const formViewData = ref({
   ]
 })
 
-const enabledState = ref(formViewData.value.basicInfo.enabledState);
+const actuatingRangeArr = ['address', 'providerAddress', 'service', 'app', 'param']
+const configItemArr = ['retries', 'timeout', 'accesslog', 'weight', '其他']
+
+const formViewEdit = ref(formViewData.value);
+
+const endOfActionOptions = [
+  {
+    label: 'provider',
+    value: 'provider'
+  },
+  {
+    label: 'consumer',
+    value: 'consumer'
+  }
+]
+
+const addConfig = () => {
+  formViewEdit.value.config.push({
+      enabledState: true,
+      endOfAction: 'provider',
+      actuatingRange: [],
+      configItem: []
+    }
+  )
+}
+
+const handleChange = (index: number, name: string) => {
+  const config = formViewData.value.config[index];
+  config[name] = config[name].filter((item) => {
+    return config[name + 'Keys'].find((i) => {
+      return i === item.key
+    })
+  })
+  config[name + 'Keys'].forEach((item) => {
+    if(!config[name].find((i) => {
+      return i.key === item
+    })) {
+      config[name].push({
+        key: item,
+        relation: '=',
+        value: ''
+      })
+    }
+  })
+}
 </script>
 
 <style lang="less" scoped>
