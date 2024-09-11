995f5c4c668636fceeb6493b4da88c79274167c7
mfordjody <11638005@qq.com>
2024-09-11 09:07:25 +0800

    [horus] Add horus monitoring query

---
+++ app/horus/core/horuser/prome.go
@@ -0,0 +1,50 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package horuser
+
+import (
+	"context"
+	apiV1 "github.com/prometheus/client_golang/api"
+	prometheusV1 "github.com/prometheus/client_golang/api/prometheus/v1"
+	"github.com/prometheus/common/model"
+	"k8s.io/klog/v2"
+	"time"
+)
+
+func (h *Horuser) InstantQuery(address, ql string, timeWindowsSecond int64) (model.Vector, error) {
+	client, err := apiV1.NewClient(apiV1.Config{Address: address})
+	if err != nil {
+		klog.Errorf("prometheus InstantQuery creating NewClient error:%v", err)
+		return nil, err
+	}
+
+	apiV1 := prometheusV1.NewAPI(client)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeWindowsSecond)*time.Second)
+	defer cancel()
+
+	result, _, err := apiV1.Query(ctx, ql, time.Now())
+	if err != nil {
+		klog.Errorf("prometheus Query error:%v url:%+v ql:%v", err, address, ql)
+		return nil, err
+	}
+
+	vector, has := result.(model.Vector)
+	if !has {
+		klog.Errorf("prometheus Query result vector error %+v", result)
+		return nil, err
+	}
+	return vector, nil
+}
