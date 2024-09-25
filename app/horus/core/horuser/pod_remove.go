95b14db73fb2ece7d37155874042a3608dc6ec9a
mfordjody <11638005@qq.com>
2024-09-25 09:45:23 +0800

    [horus] Changing the project name and adding a new pod action
    fix typo

---
+++ app/horus/core/horuser/pod_remove.go
@@ -0,0 +1,48 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package horuser
+
+import (
+	"encoding/json"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/klog/v2"
+)
+
+type SampleValue struct {
+	Operation string `json:"operation"`
+	Path      string `json:"path"`
+}
+
+func (h *Horuser) Finalizer(clusterName, podName, podNamespace string) error {
+	kubeClient := h.kubeClientMap[clusterName]
+	if kubeClient == nil {
+		klog.Errorf("Finalizer kubeClient by clusterName empty.")
+		klog.Infof("clusterName:%v podName:%v", clusterName, podName)
+		return nil
+	}
+	finalizer := SampleValue{
+		Operation: "remove",
+		Path:      "/metadata/finalizers",
+	}
+	var payload []interface{}
+	payload = append(payload, finalizer)
+	data, _ := json.Marshal(payload)
+	ctx, cancel := h.GetK8sContext()
+	defer cancel()
+	_, err := kubeClient.CoreV1().Pods(podNamespace).Patch(ctx, podName, types.JSONPatchType, data, v1.PatchOptions{})
+	return err
+}
