d6e0deab502a158d0439b507701247447599b637
mfordjody <11638005@qq.com>
2024-10-16 07:39:42 +0800

    [charts] Change files directory shell (#466)

--- manifests/charts/admin/files/helm.sh
+++ manifests/charts/admin/files/helm.sh
@@ -2,17 +2,16 @@
 
 set -eux
 
-WORKDIR=$(dirname "$0")
-WORKDIR=$(cd "$WORKDIR"; pwd)
+
 
 PROMETHEUS="https://prometheus-community.github.io/helm-charts"
 ISTIO="https://istio-release.storage.googleapis.com/charts"
 
-helm pull kube-prometheus-stack --repo $PROMETHEUS --version 65.2.0 --untar && \
-cp -r ${WORKDIR}/files/dashboards/ kube-prometheus-stack/templates/grafana/dashboards-1.14/ && \
+helm pull kube-prometheus-stack --repo "$PROMETHEUS" --version 65.2.0 --untar && \
+cp -r "/files/dashboards/" kube-prometheus-stack/templates/grafana/dashboards-1.14/ && \
 cd kube-prometheus-stack && \
-helm install kube-prometheus-stack -f values.yaml . &&\
-helm repo add istio $ISTIO && \
+helm install kube-prometheus-stack -f values.yaml . && \
+helm repo add istio "$ISTIO" && \
 helm repo update && \
 helm install istio-base istio/base -n istio-system --set defaultRevision=default && \
-helm install istiod istio/istiod -n istio-system && \
+helm install istiod istio/istiod -n istio-system
