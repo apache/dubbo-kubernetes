d61373602ff41603598c48322b2b980598f40758
mfordjody <11638005@qq.com>
2024-10-16 15:40:03 +0800

    [charts] update grafana dashboards

--- manifests/charts/admin/files/kubectl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eux
-
-JAEGER="https://raw.githubusercontent.com/istio/istio/release-1.23/samples/addons/jaeger.yaml"
-
-kubectl apply -f $JAEGER
-
-kubectl get namespace istio-system >/dev/null 2>&1 || kubectl create namespace istio-system
