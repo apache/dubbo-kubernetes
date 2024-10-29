0788679d12db360fe8f181100432aa84cef9b3ab
mfordjody <11638005@qq.com>
2024-10-29 21:08:45 +0800

    [dubboctl] Replacement of part of the project structure

--- dubboctl/pkg/deploy/common.go
+++ dubboctl/pkg/deploy/common.go
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package cmd
+package deploy
 
 import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
