2c86392da211b99e0df70535eaee391d2f25ef9e
mfordjody <11638005@qq.com>
2024-11-30 08:21:38 +0800

    [operator] add Option determinator

--- dubboctl/pkg/cli/context.go
+++ dubboctl/pkg/cli/context.go
@@ -3,13 +3,13 @@ package cli
 import "github.com/apache/dubbo-kubernetes/pkg/kube"
 
 type instance struct {
-	client map[string]kube.CliClient
+	client map[string]kube.CLIClient
 }
 
 type Context interface {
-	CliClient() (kube.CliClient, error)
+	CLIClient() (kube.CLIClient, error)
 }
 
-func (i *instance) CliClient() (kube.CliClient, error) {
+func (i *instance) CLIClient() (kube.CLIClient, error) {
 	return nil, nil
 }
---
+++ operator/cmd/cluster/shared.go
@@ -0,0 +1,45 @@
+package cluster
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+type writerPrinter struct {
+	writer io.Writer
+}
+
+type Printer interface {
+	Printf(string, ...any)
+	Println(string)
+}
+
+func NewPtrForWtr(w io.Writer) Printer {
+	return &writerPrinter{writer: w}
+}
+
+func (w writerPrinter) Printf(format string, a ...any) {
+	_, _ = fmt.Fprintf(w.writer, format, a...)
+}
+
+func (w writerPrinter) Println(s string) {
+	_, _ = fmt.Fprintln(w.writer, s)
+}
+
+func OptionDeterminator(msg string, writer io.Writer) bool {
+	for {
+		_, _ = fmt.Fprintf(writer, "%s ", msg)
+		var resp string
+		_, err := fmt.Scanln(&resp)
+		if err != nil {
+			return false
+		}
+		switch strings.ToUpper(resp) {
+		case "Y", "YES", "y", "yes":
+			return true
+		case "N", "NO", "n", "no":
+			return false
+		}
+	}
+}
