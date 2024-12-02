d295e29024a554993c1c65bba603765c479d156c
mfordjody <11638005@qq.com>
2024-12-02 12:45:33 +0800

    [operator] Add Installer and multi errors (#515)

---
+++ operator/pkg/util/dmultierr/dmultierr.go
@@ -0,0 +1,27 @@
+package dmultierr
+
+import (
+	"fmt"
+	"github.com/hashicorp/go-multierror"
+	"strings"
+)
+
+func MultiErrorFormat() multierror.ErrorFormatFunc {
+	return func(errors []error) string {
+		if len(errors) == 1 {
+			return errors[0].Error()
+		}
+		points := make([]string, len(errors))
+		for index, err := range errors {
+			points[index] = fmt.Sprintf("* %s", err)
+		}
+		return fmt.Sprintf("%d errors occurred:\n\t%s\n",
+			len(errors), strings.Join(points, "\n\t"))
+	}
+}
+
+func NewDMultiErr() *multierror.Error {
+	return &multierror.Error{
+		ErrorFormat: MultiErrorFormat(),
+	}
+}
