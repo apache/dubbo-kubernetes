ce106ec9f5827797511947d8f3b6277db2c35d11
mfordjody <11638005@qq.com>
2024-12-09 09:00:47 +0800

    [operator] Additional k8s client configuration

--- pkg/kube/client_config.go
+++ pkg/kube/client_config.go
@@ -3,6 +3,7 @@ package kube
 import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/clientcmd/api"
 )
 
 type clientConfig struct {
@@ -10,19 +11,33 @@ type clientConfig struct {
 }
 
 func (c clientConfig) RawConfig() (api.Config, error) {
-	panic("implement me")
+	cfg := api.Config{
+		Kind:           "Config",
+		APIVersion:     "v1",
+		Preferences:    api.Preferences{},
+		Clusters:       map[string]*api.Cluster{},
+		AuthInfos:      map[string]*api.AuthInfo{},
+		Contexts:       map[string]*api.Context{},
+		CurrentContext: "",
+	}
+	return cfg, nil
 }
 
 func (c clientConfig) ClientConfig() (*rest.Config, error) {
-	panic("implement me")
+	return c.copyRestConfig(), nil
+}
+
+func (c *clientConfig) copyRestConfig() *rest.Config {
+	out := c.restConfig
+	return &out
 }
 
 func (c clientConfig) Namespace() (string, bool, error) {
-	panic("implement me")
+	return "default", false, nil
 }
 
 func (c clientConfig) ConfigAccess() clientcmd.ConfigAccess {
-	panic("implement me")
+	return nil
 }
 
 func NewClientConfigForRestConfig(restConfig *rest.Config) clientcmd.ClientConfig {
