a8946870190233e2ea2daf8c8a1c2125223b7feb
mfordjody <11638005@qq.com>
2024-12-14 10:27:05 +0800

    [operator] updat upgrade and delete proto json

--- operator/cmd/cluster/upgrade.go
+++ operator/cmd/cluster/upgrade.go
@@ -2,6 +2,7 @@ package cluster
 
 import (
 	"github.com/apache/dubbo-kubernetes/dubboctl/pkg/cli"
+	//"github.com/apache/dubbo-kubernetes/operator/pkg/util/clog"
 	"github.com/spf13/cobra"
 )
 
@@ -10,22 +11,24 @@ type upgradeArgs struct {
 }
 
 func UpgradeCmd(ctx cli.Context) *cobra.Command {
-	rootArgs := &RootArgs{}
-	upArgs := &upgradeArgs{
-		installArgs: &installArgs{},
-	}
+	//rootArgs := &RootArgs{}
+	//upArgs := &upgradeArgs{
+	//	installArgs: &installArgs{},
+	//}
 	cmd := &cobra.Command{
 		Use:     "upgrade",
 		Short:   "Upgrade the Dubbo Control Plane",
 		Long:    "",
 		Example: "",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			client, err := ctx.CLIClient()
-			if err != nil {
-				return err
-			}
-			return install(client, rootArgs, upArgs.installArgs)
-		},
+		//RunE: func(cmd *cobra.Command, args []string) error {
+		//	cl := clog.NewConsoleLogger(cmd.OutOrStdout(), cmd.ErrOrStderr(), installerScope)
+		//	p := NewPrinterForWriter(cmd.OutOrStderr())
+		//	client, err := ctx.CLIClient()
+		//	if err != nil {
+		//		return err
+		//	}
+		//	return nil
+		//},
 	}
 	return cmd
 }
