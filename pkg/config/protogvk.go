932f233a25b63da5bef5d4d6600dd68666193553
mfordjody <11638005@qq.com>
2024-12-16 09:20:47 +0800

    [operator] replenish gvk proto

--- pkg/config/protogvk.go
+++ pkg/config/protogvk.go
@@ -1,7 +1,11 @@
 package config
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	gogojsonpb "github.com/gogo/protobuf/jsonpb"
+	gogoproto "github.com/gogo/protobuf/proto"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/reflect/protoreflect"
 )
@@ -15,6 +19,7 @@ type GroupVersionKind struct {
 var _ fmt.Stringer = GroupVersionKind{}
 
 func (g GroupVersionKind) String() string {
+	return g.CanonicalGroup() + "/" + g.Version + "/" + g.Kind
 }
 
 func (g GroupVersionKind) CanonicalGroup() string {
@@ -31,7 +36,7 @@ func CanoncalGroup(group string) string {
 type Spec any
 
 func ToJSON(s Spec) ([]byte, error) {
-	return toJSON()
+	return toJSON(s, false)
 }
 
 func toJSON(s Spec, pretty bool) ([]byte, error) {
@@ -41,7 +46,17 @@ func toJSON(s Spec, pretty bool) ([]byte, error) {
 	}
 	if _, ok := s.(protoreflect.ProtoMessage); ok {
 		if pb, ok := s.(proto.Message); ok {
+			b, err := MarshalIndent(pb, indent)
+			return b, err
 		}
 	}
-	return nil, nil
+	b := &bytes.Buffer{}
+	if pb, ok := s.(gogoproto.Message); ok {
+		err := (&gogojsonpb.Marshaler{Indent: indent}).Marshal(b, pb)
+		return b.Bytes(), err
+	}
+	if pretty {
+		return json.MarshalIndent(s, "", indent)
+	}
+	return json.Marshal(s)
 }
