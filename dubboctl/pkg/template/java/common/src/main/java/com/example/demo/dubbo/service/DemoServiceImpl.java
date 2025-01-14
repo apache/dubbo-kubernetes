03d01f657f5780866e36aa9a625e09d8a7ee29ad
Jian Zhong <11638005@qq.com>
2025-01-14 15:32:08 +0800

    [operator] Create supplemental logic for sdk (#536)

---
+++ dubboctl/pkg/template/java/common/src/main/java/com/example/demo/dubbo/service/DemoServiceImpl.java
@@ -0,0 +1,14 @@
+package com.example.demo.dubbo.service;
+
+import com.example.demo.dubbo.api.DemoService;
+
+import org.apache.dubbo.config.annotation.DubboService;
+
+@DubboService
+public class DemoServiceImpl implements DemoService {
+
+    @Override
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+}
