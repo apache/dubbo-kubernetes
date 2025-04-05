d79bb0c1d7d1319958aac69c90edd6a9042a6fd7
mfordjody <11638005@qq.com>
2025-04-05 12:28:36 +0800

    [charts] Finishing package configuration

--- operator/cmd/cluster/install.go
+++ operator/cmd/cluster/install.go
@@ -39,7 +39,8 @@ var InstallerScope = log.RegisterScope("installer")
 
 type installArgs struct {
 	// filenames is an array of paths to input DubboOperator CR files.
-	filenames []string
+	// TODO
+	// filenames []string
 	// sets is a string with the format "path=value".
 	sets []string
 	// waitTimeout is the maximum time to wait for all Dubbo resources to be ready.
@@ -52,14 +53,14 @@ type installArgs struct {
 
 func (i *installArgs) String() string {
 	var b strings.Builder
-	b.WriteString("filenames:    " + (fmt.Sprint(i.filenames) + "\n"))
+	// b.WriteString("filenames:    " + (fmt.Sprint(i.filenames) + "\n"))
 	b.WriteString("sets:    " + (fmt.Sprint(i.sets) + "\n"))
 	b.WriteString("waitTimeout: " + fmt.Sprint(i.waitTimeout) + "\n")
 	return b.String()
 }
 
 func addInstallFlags(cmd *cobra.Command, args *installArgs) {
-	cmd.PersistentFlags().StringSliceVarP(&args.filenames, "filenames", "f", nil, `Path to the file containing the dubboOperator's custom resources.`)
+	// cmd.PersistentFlags().StringSliceVarP(&args.filenames, "filenames", "f", nil, `Path to the file containing the dubboOperator's custom resources.`)
 	cmd.PersistentFlags().StringArrayVarP(&args.sets, "set", "s", nil, `Override dubboOperator values, such as selecting profiles, etc.`)
 	cmd.PersistentFlags().BoolVarP(&args.skipConfirmation, "skip-confirmation", "y", false, `The skipConfirmation determines whether the user is prompted for confirmation.`)
 	cmd.PersistentFlags().DurationVar(&args.waitTimeout, "wait-timeout", 300*time.Second, "Maximum time to wait for Dubbo resources in each component to be ready.")
@@ -73,12 +74,9 @@ func InstallCmdWithArgs(ctx cli.Context, rootArgs *RootArgs, iArgs *installArgs)
 		Long:  "The install command generates an Dubbo install manifest and applies it to a cluster",
 		Example: ` # Apply a default dubboctl installation.
   dubboctl install -y
-  
-  # Apply a config file.
-  dubboctl install -f dop.yaml
-  
+ 
   # Apply a default profile.
-  dubboctl install --profile=demo
+  dubboctl install --set --profile=demo -y
 		`,
 		Aliases: []string{"apply"},
 		Args:    cobra.ExactArgs(0),
@@ -105,7 +103,7 @@ func InstallCmd(ctx cli.Context) *cobra.Command {
 
 func Install(kubeClient kube.CLIClient, rootArgs *RootArgs, iArgs *installArgs, cl clog.Logger, stdOut io.Writer, p Printer) error {
 	setFlags := applyFlagAliases(iArgs.sets)
-	manifests, vals, err := render.GenerateManifest(iArgs.filenames, setFlags, cl, kubeClient)
+	manifests, vals, err := render.GenerateManifest(nil, setFlags, cl, kubeClient)
 	if err != nil {
 		return fmt.Errorf("generate config: %v", err)
 	}
