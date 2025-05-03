d78da34418329a9b2292c5202c894ebd00af48a9
mfordjody <11638005@qq.com>
2025-05-03 23:38:28 +0800

    [charts] add register plane pdb

--- operator/pkg/component/component.go
+++ operator/pkg/component/component.go
@@ -32,6 +32,7 @@ const (
 	NacosRegisterComponentName     Name = "Nacos"
 	ZookeeperRegisterComponentName Name = "Zookeeper"
 	AdminComponentName             Name = "Admin"
+	AdminExtraComponentName        Name = "Kube-Prometheus"
 )
 
 type Component struct {
@@ -70,6 +71,11 @@ var AllComponents = []Component{
 		Default:            true,
 		HelmSubDir:         "admin",
 		HelmValuesTreeRoot: "admin",
+	},
+	{
+		UserFacingName: AdminExtraComponentName,
+		Default:        true,
+		HelmSubDir:     "admin/charts/kube-prometheus",
 	},
 	{
 		UserFacingName:     NacosRegisterComponentName,
