2ff6e02da259b18dfe1436bf9582a1c9a0cabf00
mfordjody <11638005@qq.com>
2025-07-16 10:04:22 +0800

    [dubboctl] add version command

--- pkg/version/cobra.go
+++ pkg/version/cobra.go
@@ -18,25 +18,17 @@
 package version
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
 	"github.com/spf13/cobra"
-	"sigs.k8s.io/yaml"
 )
 
 type Version struct {
-	ClientVersion *BuildInfo `json:"clientVersion,omitempty" yaml:"clientVersion,omitempty"`
+	ClientVersion *BuildVersion `json:"clientVersion,omitempty" yaml:"clientVersion,omitempty"`
 }
-
-var (
-	Info BuildInfo
-)
 
 func CobraCommandWithOptions() *cobra.Command {
 	var (
 		short   bool
-		output  string
 		version Version
 	)
 	ves := &cobra.Command{
@@ -44,33 +36,14 @@ func CobraCommandWithOptions() *cobra.Command {
 		Short: "Display the information of the currently built version",
 		Long:  "The version command displays the information of the currently built version.",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if output != "" && output != "yaml" && output != "json" {
-				return errors.New(`--output must be 'yaml' or 'json'`)
+			version.ClientVersion = &Cobra
+			if short {
+				_, _ = fmt.Fprintf(cmd.OutOrStdout(), "client version: %s\n", version.ClientVersion.Version)
 			}
-			version.ClientVersion = &Info
-			switch output {
-			case "":
-				if short {
-					_, _ = fmt.Fprintf(cmd.OutOrStdout(), "client version: %s\n", version.ClientVersion.Version)
-
-				} else {
-					_, _ = fmt.Fprintf(cmd.OutOrStdout(), "client version: %s\n", version.ClientVersion.LongForm())
-				}
-			case "yaml":
-				if marshaled, err := yaml.Marshal(&version); err == nil {
-					_, _ = fmt.Fprintln(cmd.OutOrStdout(), string(marshaled))
-				}
-			case "json":
-				if marshaled, err := json.MarshalIndent(&version, "", "  "); err == nil {
-					_, _ = fmt.Fprintln(cmd.OutOrStdout(), string(marshaled))
-				}
-			}
-
 			return nil
 		},
 	}
 
 	ves.Flags().BoolVarP(&short, "short", "s", false, "Use --short=false to generate full version information")
-	ves.Flags().StringVarP(&output, "output", "o", "", "One of 'yaml' or 'json'.")
 	return ves
 }
