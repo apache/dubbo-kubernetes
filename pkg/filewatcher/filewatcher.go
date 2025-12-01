edd09c87a24995939eb93daf54060926f380025c
mfordjody <11638005@qq.com>
2025-12-01 21:01:00 +0800

    release-0.3.0

--- pkg/filewatcher/filewatcher.go
+++ pkg/filewatcher/filewatcher.go
@@ -1,7 +1,4 @@
 /*
- * Portions of this file are derived from the Istio project:
- *   https://github.com/istio/istio/blob/master/pkg/filewatcher/filewatcher.go
- *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -9,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *          http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,6 +14,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// Portions of this file are derived from the Istio project:
+//   https://github.com/istio/istio/blob/master/pkg/filewatcher/filewatcher.go
 
 package filewatcher
 
