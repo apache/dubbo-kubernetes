d64a74eed410ff394281cc33096745c7340a9333
mfordjody <11638005@qq.com>
2026-03-24 20:07:40 +0800

    Migrate tools

--- tools/scripts/update_crds.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-rm -f "../../manifests/charts/base/files/crd-all.gen.yaml"
-cp "../../api/kubernetes/customresourcedefinitions.gen.yaml" "../../manifests/charts/base/files/crd-all.gen.yaml"
